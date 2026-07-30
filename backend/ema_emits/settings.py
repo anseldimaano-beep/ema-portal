6303e1925c18a81f3e280d2e04747de998727fe7
anseldimaano-beep <anseldimaano@gmail.com>
2026-07-30 17:14:04 +0800

    Update settings.py

--- backend/ema_emits/settings.py
+++ backend/ema_emits/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 
 # Application definition
 INSTALLED_APPS = [
-INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
